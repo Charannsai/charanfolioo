81d09b3e77d49802eb79d3b3217cf434f372b804
Charannsai <pathurisai31@gmail.com>
2025-04-15 18:24:33 +0530

    feat: selection colors added

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -11,7 +11,7 @@ export default function ProjectCard({ project, index }) {
       initial={{ y: 20, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ delay: 0.1 * index }}
-      className="bg-white dark:bg-gray-400/10 rounded-lg shadow-lg overflow-hidden p-3 relative group"
+      className="bg-white dark:bg-gray-600/10 rounded-lg shadow-lg overflow-hidden p-3 relative group h-[80px]"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
@@ -32,7 +32,7 @@ export default function ProjectCard({ project, index }) {
             initial={{ opacity: 0, scale: 0.9 }}
             animate={{ opacity: 1, scale: 1 }}
             exit={{ opacity: 0, scale: 0.9 }}
-            className="absolute inset-0 bg-white dark:bg-gray-400/10 p-4 flex items-center justify-center"
+            className="absolute inset-0 bg-white dark:bg-gray-600/10 p-4 flex items-center justify-center"
           >
             <Tooltip.Provider>
               <div className="flex  gap-4">
